067873a0015875d13abe04db2fd7a6d9efba02ff
athipan1 <166430089+athipan1@users.noreply.github.com>
2026-06-21 04:12:31 +0700

    Mount Thai dashboard routes

--- app/__init__.py
+++ app/__init__.py
@@ -6,12 +6,14 @@ _OriginalFastAPI = _fastapi.FastAPI
 
 
 class ManagerFastAPI(_OriginalFastAPI):
-    """FastAPI subclass that mounts Manager operational alert routes."""
+    """FastAPI subclass that mounts Manager operational routes."""
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         from .alert_routes import router as alert_router
+        from .dashboard_routes import router as dashboard_router
         self.include_router(alert_router)
+        self.include_router(dashboard_router)
 
 
 _fastapi.FastAPI = ManagerFastAPI
